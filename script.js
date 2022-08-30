323712b8d24a2af84493ea57a66b8e6357369136
Reks <fahadadsani@gmail.com>
2022-08-30 08:00:13 +0300

    commit 2

---
+++ script.js
@@ -0,0 +1,164 @@
+/* F I R S T   E X A M P L E */
+
+
+/* console.log('Hello World'[10])
+console.log('aaa'.length) */
+
+
+/* S E C O N D   E X A M P L E */
+
+
+/*let str = 'rek'
+console.log(str[str.length -1])*/
+
+
+/* T H I R D   E X A M P L E */
+
+
+// console.log('123')
+
+
+/* F O U R T H   E X A M P L E */
+
+
+// let name = 'Fahad'
+// let isRaining = true
+// let temperature = 20
+// let planet = 'Earth'
+
+// temperature = temperature + 2
+
+// console.log(temperature)
+
+
+/* F I F T H   E X A M P L E */
+
+
+// let celsius = 10
+// let fahrenheit = celsius * 1.8 + 32
+
+// console.log(fahrenheit)
+
+
+/* S I X T H   E X A M P L E */
+
+
+// let bool = !false
+
+// console.log(bool)
+
+
+/* S E V E N T H   E X A M P L E */
+
+
+// let subscribed = false
+// let loggedIn = !false
+
+// if (subscribed === true){
+//     console.log('show the video')
+// }
+// else if(loggedIn === true){
+//     console.log('pay money u brokie')
+// }
+// else{
+//     console.log("fuck off")
+// }
+
+
+/* E I G T H   E X A M P L E */
+
+
+// let cash = 50
+// let price = 40
+// let change = cash - price
+
+// if (cash > price){
+//     console.log('u pay extra, here '+ change +' change')
+// }
+// else if (cash === price){
+//     console.log('exact change fuck off')
+// }
+// else{
+//     console.log('More money hoe '+ change)
+// }
+
+
+/* E I G H T H   E X A M P L E */
+
+
+// let cash = 50
+// let price = 40
+// let isStoreOpen = false
+
+// if (NaN){
+//     console.log('truthy boy')
+// }
+// else{
+//     console.log('falsey boy')
+// }
+
+
+
+// let val = 'fahad'
+
+// if (val){
+//     console.log(!!val)
+// }
+// else{
+//     console.log(!!val)
+// }
+
+
+/* N I N T H   E X A M P L E */
+
+
+// let hot = true
+
+// hot ? console.log('hot') : console.log('cold')
+
+
+// let cash = 50
+// let price = 40
+// let isStoreOpen = true
+
+// let str = isStoreOpen && cash>price ? 'giv' : 'no'
+
+// console.log(str)
+
+
+/* 1 0 T H   E X A M P L E */
+
+
+// let counter = 1
+
+// while (counter<=100){
+//     console.log(counter)
+//     counter+=1
+// }
+
+
+// for(let i = 0; i <= 100 ; i++){
+//     console.log(i)
+// }
+
+
+/* 1 1 T H   E X A M P L E */
+
+
+// let str = 'frontend simplified'
+
+// for (i=0;i<=str.length;i++){
+//     console.log(str[i])
+// }
+
+
+/* 1 2 T H   E X A M P L E */
+
+
+function welcomePersonToFES(name){
+    console.log(`Welcome to FES, `+ name)
+}
+
+welcomePersonToFES('david')
+welcomePersonToFES('zen')
+welcomePersonToFES('metri')
